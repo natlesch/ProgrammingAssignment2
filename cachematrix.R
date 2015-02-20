e34389b43dfc7ce79904f84015eba7d7d11e99e4
natlesch <git@schrankhome.com>
2015-02-20 11:28:03 -0600

    assignment xfer

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,32 @@
-##  -Check if solution exists and pull from cache if exists
-##  -Otherwise calculate inverse and cache result for future reference
+## Calculate the inverse of a square matrix:
+##  -Check if matrix inverse already exists and retrieve from cache if exists
+##  -Otherwise calculate inverse and cache the result for future reference
 
 ## Function to get and set cached matrix inverse
+## - Initialize with a (square) matrix
 makeCacheMatrix <- function(x = matrix()) {
+  ##Initialization
   m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
-  }
+  }  
   get <- function() x
-  setMatrixInverse <- function(mean) m <<- mean
+  
+  ##Caching
+  setMatrixInverse <- function(inverse) m <<- inverse
   getMatrixInverse <- function() m
+  
   list(set = set, get = get,
        setMatrixInverse = setMatrixInverse,
        getMatrixInverse = getMatrixInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Calculate inverse of square matrix.
+##  - Utilize instance of makeCacheMatrix for getting/setting cached inverse value
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse
   m <- x$getMatrixInverse()
   if(!is.null(m)) {
     message("getting cached matrix inverse")
